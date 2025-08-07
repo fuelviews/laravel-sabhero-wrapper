d6159571d1f4532768f2c32ea70ff36b5ec08c5a
Joshua Mitchener <64335706+thejmitchener@users.noreply.github.com>
2025-08-07 12:28:37 -0400

    Remove unused imports and deprecated bootingPackage method to simplify the service provider. (#38)

--- src/SabHeroWrapperServiceProvider.php
+++ src/SabHeroWrapperServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace Fuelviews\SabHeroWrapper;
 
-use Fuelviews\SabHeroWrapper\Models\Page;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\View;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -23,21 +20,5 @@ class SabHeroWrapperServiceProvider extends PackageServiceProvider
                     ->publishConfigFile()
                     ->publishMigrations();
             });
-    }
-
-    public function bootingPackage(): void
-    {
-        $this->publishes([
-            __DIR__.'/../database/seeders/PageTableSeeder.php' => database_path('seeders/PageTableSeeder.php'),
-        ], 'sabhero-wrapper-seeders');
-
-        View::composer('sabhero-wrapper::components.layouts.app', function ($view) {
-            $routeName = Route::currentRouteName();
-
-            $seoPage = Page::where('slug', $routeName)
-                ->first();
-
-            $view->with('seoPage', $seoPage);
-        });
     }
 }
